6739ef5cef84797b5ebc9b1e30543293eace6c71
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-09-02 02:17:49 +0600

    new commit

--- js/index.js
+++ js/index.js
@@ -21,25 +21,27 @@ const getAllData=async()=>{
 
 const displayBook=(books)=>{
 console.log(books.length)
-console.log(books)
-const newArray=books.slice(11,30)
+// console.log(books)
+const newArray=books.slice(0,20)
 total.innerText=`The number of books : ${newArray.length}`
 newArray.forEach(book => {
-    console.log(book)
-    const{title,author_name,first_publish_year}=book
-    // console.log(title)
+    // console.log(book)
+    const{title,author_name,first_publish_year,cover_i}=book
+    console.log(cover_i)
     const bookDiv=document.createElement('div')
-    bookDiv.classList.add('col-lg-4')
+    bookDiv.classList.add('col')
     
     bookDiv.innerHTML=`
     <div class="card h-100">
+    <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${title}</h5>
       <h6>${author_name}</h6>
       <h6>${first_publish_year}</h6>
-      
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
     </div>
+    
+  </div>
   </div>`
   bookContainer.appendChild(bookDiv)
 
