dab3df6ccc5ef72a3a083af4ab4872c0b8fb1e57
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-09-02 14:34:41 +0600

    new commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// taking  all needed element using id 
 const saerchField=document.getElementById('search-field')
 const bookContainer=document.getElementById('book-container')
 const spinner=document.getElementById('spinner')
@@ -5,14 +6,16 @@ const total=document.getElementById('total')
 const warning=document.getElementById('warning')
 const warning2=document.getElementById('warning2')
 
-
+// get all data from api
 const getAllData=async()=>{
   total.innerText=''
     bookContainer.innerHTML=''
     let searchText=saerchField.value
+    // empty input value handling
     if (searchText.length===0) {
       warning.classList.remove('d-none')
     }
+    // data load
    else{
     spinner.classList.remove('d-none')
     const url=`https://openlibrary.org/search.json?q=${searchText}`
@@ -21,32 +24,29 @@ const getAllData=async()=>{
     spinner.classList.add('d-none')
     saerchField.value=''
     displayBook(data.docs)
+    console.log(data.docs)
     warning.classList.add('d-none')
    }
   }
-
+// display books on ui
 const displayBook=(books)=>{
-console.log(books)
-// console.log(books)
+  // create book card and show on ui
 if (books.length>0) {
   warning2.classList.add('d-none')
-  const newArray=books.slice(0,20)
-total.innerText=`The number of books : ${newArray.length}`
-newArray.forEach(book => {
-    // console.log(book)
-    // const{title,author_name,first_publish_year,cover_i}=book
-    // console.log(cover_i)
+  const shortBooksList=books.slice(0,20)
+total.innerText=`The number of books : ${shortBooksList.length}`
+shortBooksList.forEach(book => {
+    const{title,author_name,first_publish_year,cover_i,publisher}=book
     const bookDiv=document.createElement('div')
     bookDiv.classList.add('col')
-    
     bookDiv.innerHTML=`
     <div class="card h-75">
-    <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top h-50" alt="...">
+    <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top h-50" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${book?.title}</h5>
-      <h6>Author:${book?.author_name?.[0]}</h6>
-      <h6>Published Year :${book?.first_publish_year}</h6>
-      <h6>Publisher:${book?.publisher[0]}</h6>
+      <h5 class="card-title">${title}</h5>
+      <h6>Author:${author_name?.[0]??'unknown'}</h6>
+      <h6>Published Year :${first_publish_year??'unknown'}</h6>
+      <h6>Publisher:${publisher?.[0]??'unknown'}</h6>
     </div>
     
   </div>
@@ -61,4 +61,5 @@ else{
 
 }
 
-console.log('hello')
+
+
